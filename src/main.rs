9d42a778daec5f13848ab3c828f9643e45d4e14f
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-04 20:19:44 +0200

    use markdown crate to have markdown websites

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
+#![feature(closure_to_fn_coercion)]
+extern crate markdown;
 extern crate sharp_pencil;
 
 use std::fs::File;
+use std::path::Path;
 use std::io::prelude::*;
+use markdown::file_to_html;
 use sharp_pencil::{Pencil, Request, Response, PencilResult};
 
-fn index(_: &mut Request) -> PencilResult
+fn markdown_page(name: &str) -> PencilResult
 {
-	let mut f = File::open("web/index.html").unwrap();
-	let mut contents = String::new();
-	f.read_to_string(&mut contents);
-
+	let body = file_to_html(
+		Path::new(
+			format!("web/{}.md", name).as_str()
+		)
+	).unwrap();
+	let mut f = File::open("template.html").unwrap();
+	let mut wrapper = String::new();
+	f.read_to_string(&mut wrapper).unwrap();
+	let contents = wrapper.replace("[[[contents]]]", body.as_ref());
 	Ok(Response::from(contents))
 }
 
 fn main()
 {
 	let mut app = Pencil::new("web");
+	let index = |_: &mut Request| { markdown_page("index") };
 	app.get("/", "index", index);
 	app.enable_static_file_handling();
 	app.run("127.0.0.1:80");
---
+++ web/index.md
@@ -0,0 +1,15 @@
+## About
+This website is about me, the most important
+person in the universe, right after no one.
+
+## Contribution
+Contributing to this page is permitted, just clone
+the repo, make your changes and create a pull-request.
+Tada. If your changes are at least a bit helpful,
+they might be merged into this page. No idea what would
+you want to contribute though, I am already perfect.
+
+## Contact
+* Email: luk.hozda@gmail.com
+* Skype: luciusmagn
+* Github: luciusmagn
